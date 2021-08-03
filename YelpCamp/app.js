4ef68004d6b8175d4373933075411095cb629423
rajeshtiwari1998 <tiwari1498krishna@gmail.com>
2021-08-03 15:12:39 +0530

    adding files for github pages

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -10,13 +11,14 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp", {
 });
 
 const db = mongoose.connection;
-db.on("erro", console.error.bind(console, "Connection error:"));
+db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
   console.log("Database Connected!!");
 });
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
